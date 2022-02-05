9e08ca5145aae398b54409d33b8bb5bdd129c954
Jakub Pas <jakubpas@gmail.com>
2022-02-05 20:18:33 +0100

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# Atari 6502 test
-Atari 6502 cross compiller assembler test
+# Atari 6502 Mads Assembler Test
+
+## Requirements:
+
+ - Idea with 6502 plugin
+ - MADS Assembler
+ - Atari800 emulator
+
+## Idea External tools configuration:
+
+- Set working dir to $FileDir$
+
+
+- Compile:
+
+```
+./local/bin/mads $FilePath$ -o:$FileNameWithoutExtension$.xex -p -t:$FileNameWithoutExtension$.lab -l:$FileNameWithoutExtension$.lst
+```
+
+- Run:
+
+```
+/usr/bin/xterm -iconic -e atari800 -run $FileNameWithoutExtension$.xex
+```
+
--- example.asm
+++ example.asm
@@ -3,20 +3,20 @@
     org $8000 ;Start of code
 
 begin:
-    pm = $a000 ; Player/Missile start address
-    py = 0200 ; Vertical position of player0
-    HPOSP0 = $d000   ;HPOSP0 - horizontal position of player0 (shadow registry)
-    px = $6000 ; Stores value of player0 horizontal position
-    DEL = $6001 ; Stores value of move delay
+    pm = $a000      ; Player/Missile start address
+    py = 0200       ; Vertical position of player0
+    HPOSP0 = $d000  ; HPOSP0 - horizontal position of player0 (shadow registry)
+    px = $6000      ; Stores value of player0 horizontal position
+    DEL = $6001     ; Stores value of move delay
 
 	lda >pm
-	sta $d407	;PMBASE - starszy adres obszaru z duchami
-	ldy #2 ; Show both player and missiles
-	sty $d01d ;PMCTL Player/Missile control
-	lda #$e ; Player0 color (white)
-	sta 704	; shadow registry of player0 color
-	lda #%00111010 ; Player/Missile configuration bits settings
-	sta 559		;DMACTLS - registry of Player/Missile settings
+	sta $d407	    ; PMBASE - msb of Player/Missile address
+	ldy #2          ; Show both player and missiles
+	sty $d01d       ; PMCTL Player/Missile control
+	lda #$e         ; Player0 color (white)
+	sta 704	        ; Shadow registry of player0 color
+	lda #%00111010  ; Player/Missile configuration bits settings
+	sta 559		    ; DMACTLS - registry of Player/Missile settings
 	lda #120
 	sta HPOSP0
 	sta px
@@ -26,7 +26,7 @@ loop:
 	cpx #0
 	bne loop
 	
-	ldx $278 ; Joystick position 
+	ldx $278        ; Joystick position
 	cpx #11
 	beq left 
 	cpx #7
