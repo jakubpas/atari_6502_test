3aa881207e6f831456976ef4f9e9af83fa1ffcb5
Jakub Pas <jakubpas@gmail.com>
2022-02-06 19:21:07 +0100

    player1 added

--- example.asm
+++ example.asm
@@ -3,20 +3,36 @@
     org $8000 ;Start of code
 
 begin:
+
+    RAMTOP = 106    ; Returns last page number of available ram;
     pm = $a000      ; Player/Missile start address
-    py = 0200       ; Vertical position of player0
+    py = 200        ; Player0 vertical position
+    p1y = 10        ; Player1 vertical position
     HPOSP0 = $d000  ; HPOSP0 - horizontal position of player0 (shadow registry)
+    HSPOS1 = $d001  ; HPOSP0 - horizontal position of player0 (shadow registry)
     px = $6000      ; Stores value of player0 horizontal position
     DEL = $6001     ; Stores value of move delay
 
-	lda >pm
+    lda #0
+    sta $2C8 ; Set border color
+    sta $2C5 ; Set text color
+    sta $2C6 ; Set background color
+
+	lda >pm         ; get high byte of pm
 	sta $d407	    ; PMBASE - msb of Player/Missile address
 	ldy #2          ; Show both player and missiles
 	sty $d01d       ; PMCTL Player/Missile control
 	lda #$e         ; Player0 color (white)
-	sta 704	        ; Shadow registry of player0 color
+	sta $2C0	    ; Shadow registry of player0 color
+
+	lda #123        ; Player1 color (white)
+    sta $2C1	    ; Shadow registry of player1 color
+    lda #120
+    sta HSPOS1
+
 	lda #%00111010  ; Player/Missile configuration bits settings
 	sta 559		    ; DMACTLS - registry of Player/Missile settings
+
 	lda #120
 	sta HPOSP0
 	sta px
@@ -60,4 +76,21 @@ right:
 	dta b(%11111110)
 	dta b(%10111010)
 	dta b(%10111010)
+
+	org pm + $500 + p1y
+	dta b(%10000001)
+	dta b(%10000001)
+	dta b(%10000001)
+	dta b(%10011001)
+	dta b(%10111101)
+	dta b(%11111111)
+	dta b(%11111111)
+	dta b(%11111111)
+	dta b(%11111111)
+	dta b(%10111101)
+	dta b(%10011001)
+	dta b(%10000001)
+	dta b(%10000001)
+	dta b(%10000001)
+
 	run begin
