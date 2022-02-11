4c6c8003fe594ac82d8ec76dd74ec3b6dc05ce58
Jakub Pas <jakubpas@gmail.com>
2022-02-11 01:18:32 +0100

    update comments

--- example.asm
+++ example.asm
@@ -1,4 +1,4 @@
-    org $8000 ;Start of code
+    org $8000       ;Start of code
 
 begin:
     ramtop = 106    ; Returns last page number of available ram;
@@ -21,25 +21,25 @@ begin:
     p2y = pm + $600 + p2y1
 
     lda #0          ; Get black color
-    sta $2C8        ; Set border color
-;    sta $2C5        ; Set text color
-    sta $2C6        ; Set background color
+    sta $2c8        ; Set border color
+;    sta $2c5        ; Set text color
+    sta $2c6        ; Set background color
 	lda >pm         ; get high byte of pm
 	sta $d407	    ; PMBASE - msb of Player/Missile address
 	ldy #2          ; Show both player and missiles
 	sty $d01d       ; PMCTL Player/Missile control
 	lda #$e         ; Player0 color (white)
-	sta $2C0	    ; Shadow registry of player0 color
+	sta $2c0	    ; Shadow registry of player0 color
 	lda #123        ; Player1 color (red)
-    sta $2C1	    ; Shadow registry of player1 color
+    sta $2c1	    ; Shadow registry of player1 color
     lda #20         ; Player2 color (white)
-    sta $2C2	    ; Shadow registry of player1 color
+    sta $2c2	    ; Shadow registry of player1 color
 	lda #%00111010  ; Player/Missile configuration bits settings
-	sta $22F		; DMACTLS - registry of Player/Missile settings
+	sta $22f		; DMACTLS - registry of Player/Missile settings
 	lda #%00000011  ; Enable four players + fifth player or missiles
-	sta $D01D       ; Save players configuration
+	sta $d01d       ; Save players configuration
 	lda #120        ; Player0 horizontal positon (44 - 205)
-	sta HPOSP0      ; Set Player0 horizontal positon to hadrware registry
+	sta hposp0      ; Set Player0 horizontal positon to hadrware registry
 	sta px          ; Set Player0 horizontal positon
 
     lda #<dl        ; Set up display list
@@ -48,7 +48,7 @@ begin:
     sta $231
 
 start_loop:
-    ldx $0284 ; Check fire pressed
+    ldx $0284       ; Check fire pressed
     cpx #1
     beq start_loop
     jsr start_game
@@ -56,7 +56,7 @@ start_loop:
 
 start_game:
     lda #start_lives
-    sta LIVES
+    sta lives
     lda #$15
     sta text_lives + 1
 
@@ -72,7 +72,6 @@ start_game:
     lda #>dl
     sta $231
     jsr initialize_player1
-
 
 loop:
     jsr delay
@@ -101,11 +100,11 @@ loop:
     right:
 	inc px
 	lda px
-	sta HPOSP0
+	sta hposp0
 	jmp loop
 
 get_random:
-    lda $d20a ; 44 - 205
+    lda $d20a       ; 44 - 205
     cmp #204
     bcs more_or_same
     cmp #44
@@ -119,19 +118,19 @@ get_random:
     rts
 
 delay:
-	dec DEL
-	ldx DEL
+	dec del
+	ldx del
 	cpx #0
 	bne delay
 	rts
 
 move_player1_down:
     ldx p1yi + 15   ; Player1 height
-    LOOP1:
+    loop1:
     lda $a500,x
     sta $a500,x + 1
     dex
-	bne LOOP1
+	bne loop1
 	inc p1yi
 
     lda #255
@@ -150,68 +149,68 @@ delete_player1:
     rts
 
 initialize_player1:
-    jsr get_random   ; get random position to acumulator
-    sta HSPOS1       ; Set Player1 horizontal position
+    jsr get_random  ; get random position to acumulator
+    sta hspos1      ; Set Player1 horizontal position
     lda #10
-    sta p1yi ;
-    ldx #0 ; Player1 height
-    LOOP2:
-    lda PINIT,x
-    sta P1Y,x
+    sta p1yi
+    ldx #0          ; Player1 height
+    loop2:
+    lda pinit,x
+    sta p1y,x
     inx
 	cpx #15
-	bne LOOP2
+	bne loop2
     rts
 
 initialize_player2:
     lda P2
-    cmp #0  ; if Player is already on the screen return
+    cmp #0          ; if Player is already on the screen return
     beq cont
     rts
     cont:
     lda px
-    sta $D002 ; Horizontal positoin of Player2
+    sta $d002       ; Horizontal positoin of Player2
     lda #p2y1
     sta P2
-    ldx #0 ; Player1 height
-    LOOP4:
-    lda P2INIT,x
-    sta P2Y,x
+    ldx #0          ; Player1 height
+    loop4:
+    lda p2init,x
+    sta p2y,x
     inx
 	cpx #6
-	bne LOOP4
+	bne loop4
     rts
 
 move_player2_up:
-    lda P2
+    lda p2
     cmp #0
-    bne LOOP3
+    bne loop3
     rts
-    LOOP3:
+    loop3:
     lda pm + $600 ,x
     sta pm + $600 ,x - 1
     inx
-    cpx P2 + 6
-	bne LOOP3
-	dec P2
+    cpx p2 + 6
+	bne loop3
+	dec p2
     rts
 
 increase_score:
-    inc score          ; Actual increase
-    lda score          ; Load score to Acumulator
-    ldy #$0f           ; Load ATASCII "0" to Y registry
-    ldx #$1a           ; Load ATASCII "1" to Y registry
-    sec                ; Set Carry register
+    inc score       ; Actual increase
+    lda score       ; Load score to Acumulator
+    ldy #$0f        ; Load ATASCII "0" to Y registry
+    ldx #$1a        ; Load ATASCII "1" to Y registry
+    sec             ; Set Carry register
     a:
-    iny                ; Increase Y register
-    sbc #100           ; Subtract 100 with Carry (Binary mode)
-    bcs a              ; Loop a on Carry Set
+    iny             ; Increase Y register
+    sbc #100        ; Subtract 100 with Carry (Binary mode)
+    bcs a           ; Loop a on Carry Set
     b:
-    dex                ; Decrease x
-    adc #10            ; Add 10 to x
-    bmi b              ; Branch if negative
-    adc #$0f           ; Convert dec to ATASCII
-    sty text_score     ; Store digit 1
+    dex             ; Decrease x
+    adc #10         ; Add 10 to x
+    bmi b           ; Branch if negative
+    adc #$0f        ; Convert dec to ATASCII
+    sty text_score  ; Store digit 1
     stx text_score + 1 ; Store digit 2
     sta text_score + 2 ; Store digit 3
     jsr delete_player1
@@ -222,10 +221,10 @@ decrease_lives:
     sed
     lda lives
     cld
-    and #$0f ; now do lower nibble
-    ora #16 ; make a chr
+    and #$0f        ; now do lower nibble
+    ora #16         ; make a chr
     ldx #1
-    sta  text_lives, x
+    sta text_lives, x
     jsr delete_player1
     jsr initialize_player1
     ldx lives
@@ -234,27 +233,27 @@ decrease_lives:
     rts
 
 detect_collisions:
-    lda $D00C  ; Check collisons of Player0
+    lda $d00c       ; Check collisons of Player0
     cmp #0
     beq det1
     jsr decrease_lives
     lda #1
-    sta $D01E  ; reset collisions
+    sta $d01e       ; reset collisions
     det1:
-    lda $D00E  ; Check collistio of plyer two
+    lda $d00e       ; Check collistio of plyer two
     cmp #0
     beq det2
     jsr increase_score
     lda #1
-    sta $D01E  ; reset collisions
+    sta $d01e       ; reset collisions
     det2:
     rts
 
 game_over:
     lda #1
-    sta $D01E  ; reset collisions
+    sta $d01e       ; reset collisions
     jsr delete_player1
-    lda #<dl_over        ; Set up display list`
+    lda #<dl_over   ; Set up display list`
     sta $230
     lda #>dl_over
     sta $231
@@ -277,7 +276,7 @@ game_over:
 	dta b(%10111010)
 	dta b(%10111010)
 
-	org PINIT
+	org pinit
 	dta b(%00000000)
 	dta b(%10000001)
 	dta b(%10000001)
@@ -295,7 +294,7 @@ game_over:
 	dta b(%10000001)
 	dta b(%00000000)
 
-	org P2INIT
+	org p2init
 	dta b(%00000000)
     dta b(%00010000)
     dta b(%00010000)
